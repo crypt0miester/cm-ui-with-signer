58003d0db967a766a5d6b4a14a5870b646fe1463
crypt0miester <crypt0miester@gmail.com>
2022-05-11 09:55:46 +0000

    test fix

--- functions/api/sign-me-please.js
+++ functions/api/sign-me-please.js
@@ -1,6 +1,6 @@
 import { Keypair } from '../solana/keypair';
 import { Transaction } from '../solana/transaction';
-import { jsonResponse } from "../utils/jsonResponse";
+// import { jsonResponse } from "../utils/jsonResponse";
 
 const REACT_APP_CANDY_SIGNER = [124,209,126,198,226,20,98,105,161,111,1,33,217,141,156,102,113,206,239,158,185,148,32,93,123,134,209,11,156,182,10,102,168,173,136,176,164,255,117,140,194,178,205,246,160,95,48,121,58,88,247,165,42,34,170,78,170,80,233,134,153,170,249,149]
 
@@ -28,6 +28,12 @@ export const onRequest = async (
     // get seconds 
     var seconds = Math.round(timeDiff);
     console.log(seconds + "ms");
-    return jsonResponse({txn: serializedTxn, cms: candyMachineSigner.toString()})
+
+    const jsonResponse = JSON.stringify({txn: serializedTxn, cms: candyMachineSigner.toString()}, null, 2);
+    return new Response(jsonResponse, {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    })
   }
 
