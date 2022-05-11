c0ca47d5338ab604be0933bbe9b1de924ff5ceea
crypt0miester <crypt0miester@gmail.com>
2022-05-11 10:00:13 +0000

    final fix

--- functions/api/sign-me-please.js
+++ functions/api/sign-me-please.js
@@ -1,6 +1,7 @@
-import { Keypair } from '../solana/keypair';
-import { Transaction } from '../solana/transaction';
-// import { jsonResponse } from "../utils/jsonResponse";
+import {
+    Keypair,
+    Transaction,
+  } from '@solana/web3.js';
 
 const REACT_APP_CANDY_SIGNER = [124,209,126,198,226,20,98,105,161,111,1,33,217,141,156,102,113,206,239,158,185,148,32,93,123,134,209,11,156,182,10,102,168,173,136,176,164,255,117,140,194,178,205,246,160,95,48,121,58,88,247,165,42,34,170,78,170,80,233,134,153,170,249,149]
 
@@ -9,7 +10,6 @@ const REACT_APP_CANDY_SIGNER = [124,209,126,198,226,20,98,105,161,111,1,33,217,1
 export const onRequest = async (
   event,
 ) => {
-    let startTime = new Date();
     let seed = new Uint8Array(REACT_APP_CANDY_SIGNER);
     let candyMachineSignerKey = await Keypair.fromSecretKey(seed);
     const json = await event.request.clone().json()
@@ -22,13 +22,7 @@ export const onRequest = async (
     // console.log(txn);
     const config = {verifySignatures: false}
     const serializedTxn = await txn.serialize(config)
-    let endTime = new Date();
-    let timeDiff = endTime - startTime; //in ms
   
-    // get seconds 
-    var seconds = Math.round(timeDiff);
-    console.log(seconds + "ms");
-
     const jsonResponse = JSON.stringify({txn: serializedTxn, cms: candyMachineSigner.toString()}, null, 2);
     return new Response(jsonResponse, {
       headers: {
